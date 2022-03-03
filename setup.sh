28761f813776159fbea0fa611a6e00fc96085572
ravem <transalp98@gmail.com>
2022-03-03 15:59:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -72,13 +72,14 @@ systemctl start mariadb >> $logfile 2>&1
 systemctl enable mariadb >> $logfile 2>&1
 
 # Configure SQL installation
-mysql --user=root <<_EOF_
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${rootDBpass}';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 CREATE DATABASE zabbix CHARACTER SET UTF8 COLLATE UTF8_BIN;
 CREATE USER 'zabbix'@'%' IDENTIFIED BY '${zabbixDBpass}';
-GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'%';
+GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'%' IDENTIFIED BY '${zabbixDBpass}';
 CREATE USER 'zbx_monitor'@'%' IDENTIFIED BY '${monitorDBpass}';
 GRANT USAGE,REPLICATION CLIENT,PROCESS,SHOW DATABASES,SHOW VIEW ON *.* TO 'monitorDBpass'@'%';
 FLUSH PRIVILEGES;
